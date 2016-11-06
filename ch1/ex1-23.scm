58735e1f218765d0ff0e17086e4c758546919c8f
relativity <hanchl@live.com>
2016-11-06 19:48:08 +0800

    add ex1.22-23

---
+++ ch1/ex1-23.scm
@@ -0,0 +1,44 @@
+;ex1.23
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (* test-divisor test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (next n)
+  (if (= n 2)
+    3
+    (+ n 2)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (prime? n) (= n (smallest-divisor n)))
+
+(define (find-smallest-prime-greater-than-n n count)
+  (cond ((= count 0) (display "finish"))
+        ((prime? (+ n 1)) (timed-prime-test (+ n 1)) (find-smallest-prime-greater-than-n (+ n 1) (- count 1)))
+        (else (find-smallest-prime-greater-than-n (+ n 1) count))))
+
+(define (search-for-primes n)
+  (find-smallest-prime-greater-than-n n 3))
+
+;test
+(search-for-primes 1000)
+
+(search-for-primes 10000)
+
+(search-for-primes 100000)
+
+(search-for-primes 1000000)
